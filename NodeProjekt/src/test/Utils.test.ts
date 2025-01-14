11383909b4572921fc1c314cd3e1d0bf5d3054ec
Shohrukh Koyirov <shohrukhkoyirov@gmail.com>
2025-01-14 11:57:18 +0100

    some assertion methods

--- NodeProjekt/src/test/Utils.test.ts
+++ NodeProjekt/src/test/Utils.test.ts
@@ -1,8 +1,14 @@
-import { toUpperCase } from "../app/Utils";
+import {getStringInfo, toUpperCase} from "../app/Utils";
 
 
 describe("Utils test suit", () => {
 
+    test('should return uppercase (second way to test)', () => {
+        const result = toUpperCase('abc');
+        expect(result).toBe('ABC');
+    })
+
+    // or most popular
     it('should return uppercase of valid string', () => {
         // arrange
         const sut = toUpperCase;
@@ -15,9 +21,16 @@ describe("Utils test suit", () => {
         expect(actual).toBe(expected);
     })
 
-    // or
-    test('should return uppercase (second way to test)', () => {
-        const result = toUpperCase('abc');
-        expect(result).toBe('ABC');
+    it.only('should return info for valid string', () => {
+        const actual = getStringInfo('My-String');
+
+        expect(actual.lowerCase).toBe('my-string');
+        expect(actual.extraInfo).toEqual({});
+
+        expect(actual.characters.length).toBe(9);
+        expect(actual.characters).toHaveLength(9);
+
+        expect(actual.characters).toEqual(['M', 'y', '-', 'S', 't', 'r', 'i', 'n', 'g']);
+        expect(actual.characters).toContain('M');
     })
 })
